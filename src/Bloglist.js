e7e494c453115c03290e3ce428b9e0938adf4d1d
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-24 22:18:21 -0400

    Create blog list component

---
+++ src/Bloglist.js
@@ -0,0 +1,16 @@
+const Bloglist = ({ posts, title }) => {
+
+    return (
+        <div className="post-list">
+            <h2>{title}</h2>
+            {posts.map((post) => (
+                <div className="post-preview" key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>posted by {post.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Bloglist;
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,17 @@
 import { useState } from 'react';
+import Bloglist from './Bloglist';
 
 const Home = () => {
     const [posts, setPosts] = useState([
         { title: 'Welcome to BingBlog', body: 'bingus here..', author: 'bingus', id: 1 },
-        { title: 'hello bings', body: 'welcome to my twisted ankle', author: 'floppa', id: 2},
+        { title: 'hello bings', body: 'welcome to my twisted ankle', author: 'floppa', id: 2 },
         { title: 'seize the flops', body: 'end them', author: 'bulgus', id: 3 }
     ]);
 
     return (
         <div className="home">
-            { posts.map((post) => (
-               <div className="post-preview" key={ post.id }>
-                <h2>{ post.title} </h2>
-                <p>posted by { post.author }</p>
-               </div> 
-            ))}
+            <Bloglist posts={posts} title="All posts!" />
+            <Bloglist posts={posts.filter((post) => post.author === 'bingus')} title="Bingus's posts" />
         </div>
     );
 }
