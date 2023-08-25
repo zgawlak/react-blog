908ed0b287580dd202f41f4b44cdabb060be5d41
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-24 21:56:49 -0400

    Create blog post preview and style, template

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [posts, setPosts] = useState([
+        { title: 'Welcome to BingBlog', body: 'bingus here..', author: 'bingus', id: 1 },
+        { title: 'hello bings', body: 'welcome to my twisted ankle', author: 'floppa', id: 2},
+        { title: 'seize the flops', body: 'end them', author: 'bulgus', id: 3 }
+    ]);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            { posts.map((post) => (
+               <div className="post-preview" key={ post.id }>
+                <h2>{ post.title} </h2>
+                <p>posted by { post.author }</p>
+               </div> 
+            ))}
         </div>
     );
 }
