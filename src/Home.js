a61ef882579a4b02f08479bb644146fe1ad859f5
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-26 17:18:08 -0400

    Add loading message and timeout to simulate loading time

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,24 @@ import Bloglist from './Bloglist';
 
 const Home = () => {
     const [posts, setPosts] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch("http://localhost:8000/posts")
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                setPosts(data);
-            })
+        setTimeout(() => {
+            fetch("http://localhost:8000/posts")
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setPosts(data);
+                    setIsPending(false);
+                });
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {posts && <Bloglist posts={posts} title="All posts!" />}
         </div>
     );
