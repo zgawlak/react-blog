b052787cb276f38e8fd3098c6a0b151f7f6ec86f
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-26 17:29:53 -0400

    Add catch errors, add error message

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,32 @@ import Bloglist from './Bloglist';
 const Home = () => {
     const [posts, setPosts] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch("http://localhost:8000/posts")
                 .then(res => {
+                    if(!res.ok) {
+                        throw Error('Could not fetch data');
+                    }
                     return res.json();
                 })
                 .then(data => {
                     setPosts(data);
                     setIsPending(false);
-                });
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
         }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {posts && <Bloglist posts={posts} title="All posts!" />}
         </div>
