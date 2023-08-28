d6a7eed9f0b71c650578acf9e9b49528a8edda87
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-28 18:04:54 -0400

    Reuse useFetch hook to link blog post previews to details page

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -1,13 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const PostDetails = () => {
-    const {id} = useParams();
+    const { id } = useParams();
+    const { data: post, error, isPending } = useFetch('http://localhost:8000/posts/' + id);
 
-    return ( 
+    return (
         <div className="post-details">
-            <h2>Blog Details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {post && (
+                <article>
+                    <h2>{post.title}</h2>
+                    <p>posted by {post.author}</p>
+                    <div>{post.body}</div>
+                </article>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default PostDetails;
