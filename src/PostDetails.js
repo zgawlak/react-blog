f11dc52605ae9dcc6e194d57161c14ce7b07ce16
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-29 10:46:20 -0400

    Add delete button and handler

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -1,9 +1,19 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const PostDetails = () => {
     const { id } = useParams();
     const { data: post, error, isPending } = useFetch('http://localhost:8000/posts/' + id);
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch(`http://localhost:8000/posts/${post.id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    };
 
     return (
         <div className="post-details">
@@ -14,10 +24,11 @@ const PostDetails = () => {
                     <h2>{post.title}</h2>
                     <p>posted by {post.author}</p>
                     <div>{post.body}</div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
-    );
+    )
 }
 
 export default PostDetails;
